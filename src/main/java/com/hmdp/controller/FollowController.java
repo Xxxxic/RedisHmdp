f5d46d86960c2cbea8fad70d93710a9ca00c7e90
Xxxxic <2453915974@qq.com>
2024-04-08 16:55:16 +0800

    用户关注：查询 & 关注取关

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,13 +1,15 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 虎哥
@@ -16,5 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+    @Resource
+    private IFollowService followService;
 
+    //判断当前用户是否关注了该博主
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long followUserId) {
+        return followService.isFollow(followUserId);
+    }
+
+    //实现取关/关注
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long followUserId, @PathVariable("isFollow") Boolean isFellow) {
+        return followService.follow(followUserId, isFellow);
+    }
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,14 +1,17 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +20,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    // TODO: 缓存优化 - 直接数据库
+
+    /**
+     * 判断当前用户是否关注了该博主
+     *
+     * @param followUserId 判断被关注的博主
+     */
+    @Override
+    public Result isFollow(Long followUserId) {
+        Long userid = UserHolder.getUser().getId();
+        Integer count = query().eq("user_id", userid).
+                eq("follow_user_id", followUserId).count();
+        return Result.ok(count > 0);
+    }
+
+    /**
+     * 实现取关/关注
+     *
+     * @param followUserId 将操作博主的id
+     * @param isFollow     当前是否已经关注
+     */
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        Long userid = UserHolder.getUser().getId();
+        //未关注：关注
+        if (!isFollow) {
+            Follow follow = new Follow();
+            follow.setUserId(userid);
+            follow.setFollowUserId(followUserId);
+            save(follow);
+        } else { //已关注：取关
+            remove(new LambdaQueryWrapper<Follow>().eq(Follow::getUserId, userid)
+                    .eq(Follow::getFollowUserId, followUserId));
+        }
+        return Result.ok();
+    }
 }
